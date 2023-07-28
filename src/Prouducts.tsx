75dba5397bc8733bb3059540a6ffc8fcbc958eb6
rawanBairouti <rawan.bairouti@gmail.com>
2023-07-27 22:37:41 -0700

    homepage done for now - no functionality but products are rendered dynamically

--- src/Prouducts.tsx
+++ src/Prouducts.tsx
@@ -1,15 +1,46 @@
+import product1 from './assets/product1.png';
+import product2 from './assets/product2.png';
+import product3 from './assets/product3.png';
+import product4 from './assets/product4.png';
 import Product from './components/Product';
 import './Products.css';
 
 export default function Products() {
-    const products = Array(4);
+    const products = [
+		{
+			image: product1,
+            brand: 'Off-White',
+            name: 'Out Of Office "Ooo" sneakers',
+            price: 775,
+        },
+        {
+			image: product2,
+            brand: 'Nike',
+            name: 'Nike Gamma Force',
+            price: 200,
+        },
+        {
+			image: product3,
+            brand: 'Nike',
+            name: 'Cosmic Unity 3',
+            price: 160,
+        },
+        {
+			image: product4,
+            brand: 'adidas',
+            name: 'DAILY 3.0 SHOES',
+            price: 98.99,
+        },
+    ];
 
-    products.fill(<Product />);
+	const productsArray = products.map(product => 
+		<Product key={product.name} image={product.image} brand={product.brand} name={product.name} price={product.price} />
+	)
 
     return (
         <div className='products'>
             <h1>Explore our latest drops</h1>
-            <div className='products__container'>{products}</div>
+            <div className='products__container'>{productsArray}</div>
         </div>
     );
 }
